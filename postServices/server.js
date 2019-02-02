effc230c4155bfa9f4998c8a6c7a2320e33bf714
mohamed gassama <bouyagassama@gmail.com>
2019-02-01 22:58:13 -0500

    Setup mongodb and dockerfile

--- postServices/server.js
+++ postServices/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const logger = require('morgan');
 
 const server =  express();
 
 const port = process.env.PORT || 3002;
+
+// Express middleware
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+server.use(logger('dev'));
+
+// DB config 
+const db = require('./config/keys_dev').mongoURL
+
+// Connect to MongoDB 
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+
+
 
 server.get('/', async (req, res) => {
    await res.json({hello: "Up and running "});
